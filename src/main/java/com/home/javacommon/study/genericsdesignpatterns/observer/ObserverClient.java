f7a59fb6cb581d92b5091e47a93ba2f567e4863c
nvd11 <nvd11@163.com>
2022-10-09 01:14:38 +0800

    test git

--- src/main/java/com/home/javacommon/study/genericsdesignpatterns/observer/ObserverClient.java
+++ src/main/java/com/home/javacommon/study/genericsdesignpatterns/observer/ObserverClient.java
@@ -1,8 +1,6 @@
 package com.home.javacommon.study.genericsdesignpatterns.observer;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashSet;
